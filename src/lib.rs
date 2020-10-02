385cd895b14092fa4c6a7215225bfd297d319344
Stephan Beyer <s-beyer@gmx.net>
2020-10-03 01:17:36 +0200

    Add Problem struct + substructs and Problem::read_from_file()

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,106 @@ use std::error::Error;
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("Content:\n{}", contents);
+    Problem::read_from_file(config.filename)?;
 
     Ok(())
+}
+
+pub struct Position {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Position {
+    pub fn new(x: f64, y: f64) -> Position {
+        Position { x: x, y: y }
+    }
+}
+
+pub struct Depot {
+    pub position: Position,
+    pub start_level: i32,
+    pub daily_production: i32,
+    pub daily_cost: f64,
+}
+
+pub struct Customer {
+    pub id: i32,
+    pub position: Position,
+    pub start_level: i32,
+    pub max_level: i32,
+    pub min_level: i32,
+    pub daily_consumption: i32,
+    pub daily_cost: f64,
+}
+
+pub struct Problem {
+    pub num_nodes: i32,
+    pub num_days: i32,
+    pub num_vehicles: i32,
+    pub capacity: i32,
+    pub depot: Depot,
+    pub customers: Vec<Customer>,
+}
+
+impl Problem {
+    pub fn read_from_file(filename: String) -> Result<Problem, Box<dyn Error>> {
+        let contents = fs::read_to_string(filename)?;
+        let mut iter = contents.split_whitespace();
+
+        let num_nodes = iter.next().unwrap().parse::<i32>()?;
+        let num_days = iter.next().unwrap().parse::<i32>()?;
+        let capacity = iter.next().unwrap().parse::<i32>()?;
+        let num_vehicles = iter.next().unwrap().parse::<i32>()?;
+        let depot_id = iter.next().unwrap().parse::<i32>()?;
+        assert!(depot_id == 0, "Depot identifier is not 0");
+        let depot_x = iter.next().unwrap().parse::<f64>()?;
+        let depot_y = iter.next().unwrap().parse::<f64>()?;
+        let depot_level = iter.next().unwrap().parse::<i32>()?;
+        let depot_production = iter.next().unwrap().parse::<i32>()?;
+        let depot_cost = iter.next().unwrap().parse::<f64>()?;
+
+        println!("Content:\n{}", contents);
+
+        let mut customers = Vec::new();
+        let mut index = 0;
+        while index < num_nodes - 1 {
+            index += 1;
+            let c_id = iter.next().unwrap().parse::<i32>()?;
+            assert!(c_id == index, "Customer does not have expected index");
+            let c_x = iter.next().unwrap().parse::<f64>()?;
+            let c_y = iter.next().unwrap().parse::<f64>()?;
+            let c_start_level = iter.next().unwrap().parse::<i32>()?;
+            let c_max_level = iter.next().unwrap().parse::<i32>()?;
+            let c_min_level = iter.next().unwrap().parse::<i32>()?;
+            let c_consumption = iter.next().unwrap().parse::<i32>()?;
+            let c_cost = iter.next().unwrap().parse::<f64>()?;
+
+            customers.push(Customer {
+                id: c_id,
+                position: Position::new(c_x, c_y),
+                start_level: c_start_level,
+                max_level: c_max_level,
+                min_level: c_min_level,
+                daily_consumption: c_consumption,
+                daily_cost: c_cost,
+            });
+        }
+
+        Ok(Problem {
+            num_nodes: num_nodes,
+            num_days: num_days,
+            num_vehicles: num_vehicles,
+            capacity: capacity,
+            depot: Depot {
+                position: Position::new(depot_x, depot_y),
+                start_level: depot_level,
+                daily_production: depot_production,
+                daily_cost: depot_cost,
+            },
+            customers: customers,
+        })
+    }
 }
 
 pub struct Config {
