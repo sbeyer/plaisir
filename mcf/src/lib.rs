40b13173c0c3400327df39b09bba2d6f1daec95b
Stephan Beyer <s-beyer@gmx.net>
2020-10-11 01:55:45 +0200

    Add minilp dependency to mcf package

--- mcf/src/lib.rs
+++ mcf/src/lib.rs
@@ -10,4 +10,19 @@ mod tests {
         assert_eq!(graph.node_count(), 3);
         assert_eq!(graph.edge_count(), 2);
     }
+
+    #[test]
+    fn minilp_works() {
+        let mut problem = minilp::Problem::new(minilp::OptimizationDirection::Maximize);
+        let x = problem.add_var(1.0, (0.0, f64::INFINITY));
+        let y = problem.add_var(2.0, (0.0, 3.0));
+
+        problem.add_constraint(&[(x, 1.0), (y, 1.0)], minilp::ComparisonOp::Le, 4.0);
+        problem.add_constraint(&[(x, 2.0), (y, 1.0)], minilp::ComparisonOp::Ge, 2.0);
+
+        let solution = problem.solve().unwrap();
+        assert_eq!(solution.objective(), 7.0);
+        assert_eq!(solution[x], 1.0);
+        assert_eq!(solution[y], 3.0);
+    }
 }
