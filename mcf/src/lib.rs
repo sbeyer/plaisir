91df15ec646d8facbcf1058f0a54b35fefe430ff
Stephan Beyer <s-beyer@gmx.net>
2020-10-11 01:33:53 +0200

    Add petgraph dependency to mcf package

--- mcf/src/lib.rs
+++ mcf/src/lib.rs
@@ -1,7 +1,13 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn petgraph_works() {
+        let mut graph = petgraph::Graph::<i32, i32, petgraph::Directed>::new();
+        let s = graph.add_node(2);
+        let v = graph.add_node(0);
+        let t = graph.add_node(-2);
+        graph.extend_with_edges(&[(s, v), (v, t)]);
+        assert_eq!(graph.node_count(), 3);
+        assert_eq!(graph.edge_count(), 2);
     }
 }
