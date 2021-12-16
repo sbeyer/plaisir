f894712d7d13573c15f144cd4366f70e31231007
Stephan Beyer <s-beyer@gmx.net>
2021-12-16 03:47:04 +0100

    Add lkh stub as safe wrapper for lkh-sys

---
+++ lkh/src/lib.rs
@@ -0,0 +1,20 @@
+use lkh_sys::NodeCoords as Coords;
+use std::os::raw::c_int;
+
+pub fn run(dimension: usize) -> Vec<usize> {
+    // dummy code that will be replaced
+    let mut data = Vec::<Coords>::new();
+    for i in 0..dimension {
+        data.push(Coords {
+            x: 0.0,
+            y: i as u64 as f64,
+        });
+    }
+
+    let tour: *const c_int = unsafe { lkh_sys::run(dimension as c_int, data.as_ptr()) };
+    // Note that ownership of tour is kept by LKH (it's a global variable there)
+
+    (0..dimension)
+        .map(|i| unsafe { *tour.add(i) } as usize - 1)
+        .collect()
+}
