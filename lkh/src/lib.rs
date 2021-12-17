05d9bed1b5ebc24337c03d796461e06c38271f32
Stephan Beyer <s-beyer@gmx.net>
2021-12-17 02:34:34 +0100

    Make lkh::run() handle trivial cases

--- lkh/src/lib.rs
+++ lkh/src/lib.rs
@@ -2,18 +2,25 @@ use lkh_sys::NodeCoords as Coords;
 use std::os::raw::c_int;
 
 pub fn run(coords: &[(usize, f64, f64)]) -> Vec<usize> {
-    // dummy code that will be replaced
-    let data: Vec<Coords> = coords
-        .iter()
-        .map(|(_, x, y)| Coords { x: *x, y: *y })
-        .collect();
+    if coords.len() <= 3 {
+        // handle trivial cases that lkh_sys will not handle
+        coords.iter().map(|(id, _, _)| *id).collect()
+    } else {
+        // convert data for lkh_sys::run()
+        let data: Vec<Coords> = coords
+            .iter()
+            .map(|(_, x, y)| Coords { x: *x, y: *y })
+            .collect();
 
-    let tour: *const c_int = unsafe { lkh_sys::run(coords.len() as c_int, data.as_ptr()) };
-    // Note that ownership of tour is kept by LKH (it's a global variable there)
+        // invoke lkh_sys::run()
+        let tour: *const c_int = unsafe { lkh_sys::run(coords.len() as c_int, data.as_ptr()) };
+        // Note that ownership of tour is kept by LKH (it's a global variable there)
 
-    (0..coords.len())
-        .map(|i| coords[unsafe { *tour.add(i) } as usize - 1].0)
-        .collect()
+        // prepare return vector
+        (0..coords.len())
+            .map(|i| coords[unsafe { *tour.add(i) } as usize - 1].0)
+            .collect()
+    }
 }
 
 #[cfg(test)]
@@ -32,6 +39,30 @@ mod tests {
                 site
             );
         }
+    }
+
+    #[test]
+    fn run_on_empty_input() {
+        let coords = vec![];
+        verify(&coords);
+    }
+
+    #[test]
+    fn run_on_single_site() {
+        let coords = vec![(512, 1.34, 3.14)];
+        verify(&coords);
+    }
+
+    #[test]
+    fn run_on_two_sites() {
+        let coords = vec![(512, 1.34, 3.14), (256, 4.13, 3.41)];
+        verify(&coords);
+    }
+
+    #[test]
+    fn run_on_three_sites() {
+        let coords = vec![(512, 1.34, 3.14), (256, 4.13, 3.41), (17, 4.31, 1.43)];
+        verify(&coords);
     }
 
     #[test]
