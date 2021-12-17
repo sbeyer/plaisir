a32c1f486c4f58f7e3766771aee46bd415ba939c
Stephan Beyer <s-beyer@gmx.net>
2021-12-17 02:09:17 +0100

    Let lkh::run() take real input data

--- lkh/src/lib.rs
+++ lkh/src/lib.rs
@@ -1,20 +1,17 @@
 use lkh_sys::NodeCoords as Coords;
 use std::os::raw::c_int;
 
-pub fn run(dimension: usize) -> Vec<usize> {
+pub fn run(coords: &[(usize, f64, f64)]) -> Vec<usize> {
     // dummy code that will be replaced
-    let mut data = Vec::<Coords>::new();
-    for i in 0..dimension {
-        data.push(Coords {
-            x: 0.0,
-            y: i as u64 as f64,
-        });
-    }
+    let data: Vec<Coords> = coords
+        .iter()
+        .map(|(_, x, y)| Coords { x: *x, y: *y })
+        .collect();
 
-    let tour: *const c_int = unsafe { lkh_sys::run(dimension as c_int, data.as_ptr()) };
+    let tour: *const c_int = unsafe { lkh_sys::run(coords.len() as c_int, data.as_ptr()) };
     // Note that ownership of tour is kept by LKH (it's a global variable there)
 
-    (0..dimension)
+    (0..coords.len())
         .map(|i| unsafe { *tour.add(i) } as usize - 1)
         .collect()
 }
@@ -25,12 +22,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn run_on_ten_sites() {
-        let tour = run(10);
+    fn run_on_many_sites() {
+        let coords = vec![
+            (12, 0.0, 0.0),
+            (42, 1.0, 0.0),
+            (7, 1.0, 1.0),
+            (113, 0.5, 1.5),
+            (2, 0.0, 2.0),
+            (13, 0.0, 1.0),
+            (29, 0.0, 0.5),
+        ];
+        let tour = run(&coords);
+        assert_eq!(tour.len(), coords.len());
+
         println!("Resulting tour:");
         for site in tour.iter() {
             println!(" * {}", site);
         }
-        assert_eq!(tour.len(), 10);
     }
 }
