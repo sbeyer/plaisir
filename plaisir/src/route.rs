60b304d314b729f95bd5f1bb5f696486cdb9feb8
Stephan Beyer <s-beyer@gmx.net>
2022-01-20 10:35:49 +0100

    Simplify trivial-handling route::Solver::solve() code

--- plaisir/src/route.rs
+++ plaisir/src/route.rs
@@ -7,12 +7,13 @@ impl Solver {
     pub fn solve(problem: &Problem, deliveries: &Deliveries, t: DayId, v: VehicleId) -> Vec<usize> {
         let mut visited_sites = deliveries.get_all_delivered_customers(t, v);
 
-        if visited_sites.is_empty() {
-            vec![0usize]
-        } else {
-            visited_sites.push(0); // add depot
+        match visited_sites.len() {
+            0 => vec![0usize],
+            1 => vec![0usize, visited_sites[0] as usize],
+            2 => vec![0usize, visited_sites[0] as usize, visited_sites[1] as usize],
+            _ => {
+                visited_sites.push(0); // add depot
 
-            if visited_sites.len() > 3 {
                 let tsp_instance = visited_sites
                     .iter()
                     .map(|site| {
@@ -28,14 +29,6 @@ impl Solver {
                     .position(|site| *site == 0)
                     .expect("Depot is expected to be in TSP tour");
                 tsp_tour.rotate_left(depot_position);
-
-                tsp_tour
-            } else {
-                let mut tsp_tour = visited_sites
-                    .iter()
-                    .map(|site| *site as usize)
-                    .collect::<Vec<_>>();
-                tsp_tour.sort_unstable();
 
                 tsp_tour
             }
