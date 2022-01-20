e89bd8108d50437889b95b097cb62e3cccbb3e41
Stephan Beyer <s-beyer@gmx.net>
2022-01-20 10:31:52 +0100

    Avoid calling LKH for trivial routes

--- plaisir/src/route.rs
+++ plaisir/src/route.rs
@@ -6,24 +6,39 @@ pub struct Solver {}
 impl Solver {
     pub fn solve(problem: &Problem, deliveries: &Deliveries, t: DayId, v: VehicleId) -> Vec<usize> {
         let mut visited_sites = deliveries.get_all_delivered_customers(t, v);
-        visited_sites.push(0); // add depot
 
-        let tsp_instance = visited_sites
-            .iter()
-            .map(|site| {
-                let site = problem.site(*site);
-                let pos = &site.position();
-                (site.id() as usize, pos.x, pos.y)
-            })
-            .collect::<Vec<_>>();
-        let mut tsp_tour = lkh::run(&tsp_instance);
+        if visited_sites.is_empty() {
+            vec![0usize]
+        } else {
+            visited_sites.push(0); // add depot
 
-        let depot_position = tsp_tour
-            .iter()
-            .position(|site| *site == 0)
-            .expect("Depot is expected to be in TSP tour");
-        tsp_tour.rotate_left(depot_position);
+            if visited_sites.len() > 3 {
+                let tsp_instance = visited_sites
+                    .iter()
+                    .map(|site| {
+                        let site = problem.site(*site);
+                        let pos = &site.position();
+                        (site.id() as usize, pos.x, pos.y)
+                    })
+                    .collect::<Vec<_>>();
+                let mut tsp_tour = lkh::run(&tsp_instance);
 
-        tsp_tour
+                let depot_position = tsp_tour
+                    .iter()
+                    .position(|site| *site == 0)
+                    .expect("Depot is expected to be in TSP tour");
+                tsp_tour.rotate_left(depot_position);
+
+                tsp_tour
+            } else {
+                let mut tsp_tour = visited_sites
+                    .iter()
+                    .map(|site| *site as usize)
+                    .collect::<Vec<_>>();
+                tsp_tour.sort_unstable();
+
+                tsp_tour
+            }
+        }
     }
 }
